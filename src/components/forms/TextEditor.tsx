b9f5ab94ebfffc97b06a17266e6f927f1b642ad3
John Omoseni <johnnyomoseni100@gmail.com>
2024-11-05 01:30:48 +0100

    chore: removed unused vars in TextEditor

--- src/components/forms/TextEditor.tsx
+++ src/components/forms/TextEditor.tsx
@@ -2,50 +2,45 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 type TextEditorProps = {
-  onHandleChange?: any;
-  value?: any;
+	onHandleChange?: any;
+	value?: any;
 };
 
 const TextEditor = ({ onHandleChange, value }: TextEditorProps) => {
-  const handleEditorChange = (
-    content: any,
-    delta: any,
-    source: any,
-    editor: ReactQuill.UnprivilegedEditor
-  ) => {
-    onHandleChange(content);
-  };
+	const handleEditorChange = (content: any) => {
+		onHandleChange(content);
+	};
 
-  return (
-    <ReactQuill
-      theme="snow"
-      value={value}
-      onChange={handleEditorChange}
-      placeholder="Write something awesome..."
-      style={{ height: "200px", marginBlock: "0.6rem 3rem" }}
-    />
-  );
+	return (
+		<ReactQuill
+			theme="snow"
+			value={value}
+			onChange={handleEditorChange}
+			placeholder="Write something awesome..."
+			style={{ height: "200px", marginBlock: "0.6rem 3rem" }}
+		/>
+	);
 };
 
 TextEditor.modules = {
-  toolbar: [
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [{ list: "ordered" }, { list: "bullet" }],
-    ["link", "image", "video"],
-    ["clean"],
-  ],
+	toolbar: [
+		["bold", "italic", "underline", "strike", "blockquote"],
+		[{ list: "ordered" }, { list: "bullet" }],
+		["link", "image", "video"],
+		["clean"],
+	],
 };
 
 TextEditor.formats = [
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "link",
-  "image",
-  "video",
+	"bold",
+	"italic",
+	"underline",
+	"strike",
+	"blockquote",
+	"list",
+	"bullet",
+	"link",
+	"image",
+	"video",
 ];
 export default TextEditor;
